f9ff0c8f0f695295b1d8ea4b52dcd915b7ad4ec9
vinayakvivek <vinayakdeepu@gmail.com>
2019-03-26 09:57:55 +0530

    Add utility functions and files

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
 #include <iostream>
+#include <string>
+
+#include "pointcloud.h"
 
 
-int main() {
-    int n = 10;
-    std::cout << n << "\n";
+int main(int argc, char** argv) {
+
+    std::string scene_file;
+
+    if (argc > 1) {
+        scene_file = argv[1];
+    } else {
+        std::cout << "[error] please provide path to scene file.\n";
+        return -1;
+    }
+
+    std::cout << scene_file << "\n";
     return 0;
 }
