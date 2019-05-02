7f23d7e014c5c2f80abd608631869a3afbbeb26c
Savyaraj <savya.deshmukh@gmail.com>
2019-05-02 11:24:47 +0530

    Update pointcloud.cpp

--- src/pointcloud.cpp
+++ src/pointcloud.cpp
@@ -15,8 +15,9 @@ Pointcloud::Pointcloud(string filename) {
 	int i = 0;
     while (fp_in.good()) {
         string line;
+        i++;
         utilityCore::safeGetline(fp_in, line);
-        if (!line.empty()) {
+        if (!line.empty() && i%30==0) {
             vector<string> tokens = utilityCore::tokenizeString(line);
 			glm::vec4 pt(atoi(tokens[2].c_str()), atoi(tokens[0].c_str()), atoi(tokens[1].c_str()), 1.0);
 			points.push_back(pt);
